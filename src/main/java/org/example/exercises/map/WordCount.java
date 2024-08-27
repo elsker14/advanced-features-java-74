6354ebff79546fb8a60e4d0a5b3698a0aa892d81
Iancu Jianu <iancujianu98@gmail.com>
2024-08-27 20:57:31 +0300

    created solution framework for frequency counter

---
+++ src/main/java/org/example/exercises/map/WordCount.java
@@ -0,0 +1,23 @@
+package org.example.exercises.map;
+
+/*
+    Word Frequency Counter:
+    Write a program that takes a sentence as input and counts the frequency of each word using a HashMap.
+ */
+
+public class WordCount {
+
+    public static void main(String[] args) {
+        /* Citirea propozitiei de la tastatura folosind Scanner */
+
+        /* Apelam metoda countWordFrequency() pentru a calcula frecventa aparitiei cuvintelor */
+
+        /* Afisare a mapei (poate fi o metoda - display()) */
+
+        /* Adaptam algoritmul pentru TreeMap astfel incat sa sortam elementele dupa cheie/valoare */
+    }
+
+    /* countWordFrequency() - split */
+
+    /* display() */
+}
