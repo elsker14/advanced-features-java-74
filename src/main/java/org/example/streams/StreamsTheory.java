0ada47c277746a2fe7575f54b6bd1d5a26346334
Iancu Jianu <iancujianu98@gmail.com>
2024-08-29 19:13:06 +0300

    example of stream with sort and filter

--- src/main/java/org/example/streams/StreamsTheory.java
+++ src/main/java/org/example/streams/StreamsTheory.java
@@ -25,6 +25,7 @@ package org.example.streams;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class StreamsTheory {
 
@@ -32,18 +33,55 @@ public class StreamsTheory {
         // 1st example - sorting
         List<String> names = Arrays.asList("Alice", "Bobo", "Anna", "Beatrice");
 
-        List<String> names2 = new ArrayList<>();
-        names2.add("Alice");
-        names2.add("Bobo");
-        names2.add("Anna");
-        names2.add("Beatrice");
-
         List<String> orderedNames = names
                 .stream()
                 .sorted()
                 .toList();
+        // List -> Stream -> Stream -> List
 
         System.out.println("Lista dezordonata: " + names);
         System.out.println("Lista ordonata: " + orderedNames);
+        System.out.println("-----------------------------------------------");
+
+        List<String> names2 = new ArrayList<>();
+        names2.add("Alice");
+        names2.add("Bob");
+        names2.add("Anna");
+        names2.add("Beatrice");
+        names2.add("Joe");
+
+        /* Pas 1: Operatia de conversie a structurii de date la tipul de date generic pentru secvente -> Stream */
+        Stream<String> streamOfNames = names2.stream();
+
+        /* Pas 2: Operatiile intermediare (filter, map, distinct, sorted, etc..) */
+        Stream<String> filteredStreamOfNamesWith3Letters = streamOfNames.filter(it -> it.length() == 3);
+        // putem avea oricate operatii intermediare vrem, toate avand ca tip de date Stream
+        // pentru ca toate prelucrarile se fac sub aceeasi tipologie
+
+        /* Pas 3: Operatia terminala -> facem conversia de la Stream la tipul de date al rezultatului pe care ni-l dorim */
+        List<String> listOfNamesWith3Letters = filteredStreamOfNamesWith3Letters.toList();
+        System.out.println("List with streams: " + listOfNamesWith3Letters);
+
+        /* Echivalentul algoritmului in mod iterativ */
+            // Alice - 0, Bob - 1, Anna - 2, Beatrice - 3, Joe - 4
+            // declaram o sublista ca si rezultat final al filtrarii
+            // parcurgem lista cu un FOR(0,n-1)
+            // vom avea o instructiune conditionala IF(it.length() == 3)
+            // daca e adevarat, stocam elementele in sublista
+            // afisam lista
+        List<String> result = new ArrayList<>();
+        for(String it: names2) {
+            if(it.length() == 3) {
+                result.add(it);
+            }
+        }
+        System.out.println("List iterativ: " + result);
+
+        /* Echivalentul algoritmului in mod stream pipeline */
+        List<String> finalListOfNames = names2
+                .stream()
+                .filter(it -> it.length() == 3)
+                .toList();
+        System.out.println("List with streams pipeline: " + finalListOfNames);
     }
 }
